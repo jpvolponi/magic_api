68095de586cc9d114c60a7b4a264dad4949b434c
jpvolponi <pierre.volponi@gmail.com>
2022-08-01 08:03:01 -0300

    update

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+type card struct {
+	id          int
+	name        string
+	category    string
+	element     string
+	atack_pwr   int
+	defence_pwr int
+	description string
+	quote       string
+}
